95734be99d9f663feb8431fb80d2999b2c80a99b
dabeatty <dabeatty@df859540-3b26-0410-a7b4-f5fa74f3971a>
2009-03-22 00:09:08 +0000

    Old latex notes from wavelet study

---
+++ wavelets/LATEX/basicwave/basicwave/images.pl
@@ -0,0 +1,90 @@
+# LaTeX2HTML 99.2beta8 (1.43)
+# Associate images original text with physical files.
+
+
+$key = q/(A|D);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="48" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$ (A\vert D)$">|; 
+
+$key = q/xasth=sum_{l}x_{k-l}h_{k};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="133" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$ x\ast
+h=\sum_{l}x_{k-l}h_{k}$">|; 
+
+$key = q/ninlbrack0,M);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="78" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$ n\in \lbrack 0,M)$">|; 
+
+$key = q/foralljinlbrack0,N);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="82" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ \forall j\in \lbrack 0,N)$">|; 
+
+$key = q/Orightarrow(A|D);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="86" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ O\rightarrow (A\vert D)$">|; 
+
+$key = q/D_{i,}=A_{i-1}astW;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="114" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$ D_{i }=A_{i-1}\ast W$">|; 
+
+$key = q/A_{0}=f;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="56" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$ A_{0}=f$">|; 
+
+$key = q/foralliinlbrack0,M);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="83" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$ \forall i\in \lbrack 0,M)$">|; 
+
+$key = q/y_{i,}+=x_{n}cdoth_{j,};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="102" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$ y_{i }+=x_{n}\cdot h_{j }$">|; 
+
+$key = q/{displaymath}BRightarrowarray{{cc}H<col_mark>D<row_mark>A<col_mark>Varray{{displaymath};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="321" HEIGHT="102" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="\begin{displaymath}B\Rightarrow
+\begin{array}{cc}
+H &amp; D \\\\
+A &amp; V
+\end{array}
+\end{displaymath}">|; 
+
+$key = q/A_{i}=A_{i-1}astV;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="106" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$ A_{i}=A_{i-1}\ast V$">|; 
+
+$key = q/foralliinlbrack1,L);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="76" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$ \forall i\in \lbrack 1,L)$">|; 
+
+$key = q/O(n^{2});MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="48" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$ O(n^{2})$">|; 
+
+1;
+
